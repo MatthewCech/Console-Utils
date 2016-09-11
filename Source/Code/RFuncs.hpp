d344c7e902caddd37402890d23d7c57f05105851
Johannes Peter <joshadow@gmail.com>
2016-09-10 22:19:20 -0700

    added pragma to ignore warning

--- Source/Code/RFuncs.hpp
+++ Source/Code/RFuncs.hpp
@@ -1,7 +1,15 @@
 #pragma once
+#include "Definitions.hpp"
+
+#ifdef OS_WINDOWS
+// VS complains about unused functions.
+#pragma warning(disable: 4505) //Unreferenced local function has been removed
+#endif
 
 namespace RFuncs
 {
+
+
   //Absolute value of int.
   static int Abs(int x)
   {
