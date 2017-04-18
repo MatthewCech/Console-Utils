ffb5a90583f9aedca8848f5563e28d88a253caef
GHOST\rever <reveriewisp@gmail.com>
2017-04-18 00:33:28 -0700

    = trimming extra out

--- Source/Dependencies/rlutil/Includes/rlutil.h
+++ Source/Dependencies/rlutil/Includes/rlutil.h
@@ -10,10 +10,9 @@
  * About: Copyright
  * (C) 2010 Tapio Vierros
  *
- * About: Licensing (DWTFYW)
- * See <License>    (DWTFYW)
+ * About: Under a "Do whatever the F*** you want" Licence for this file specifically.
  *
- * Minor tweaks to this specific file: 2016 Reverie Wisp
+ * Modified for just this app by Reverie Wisp
  */
 
 
@@ -52,65 +51,6 @@
 		#define RLUTIL_INLINE __inline__
 	#endif
 #endif
-
-#ifdef _WIN32
-	#include <windows.h>  // for WinAPI and Sleep()
-	#define _NO_OLDNAMES  // for MinGW compatibility
-	#include <conio.h>    // for getch() and kbhit()
-	#define getch _getch
-	#define kbhit _kbhit
-#else
-	#ifdef __cplusplus
-		#include <cstdio> // for getch()
-	#else // __cplusplus
-		#include <stdio.h> // for getch()
-	#endif // __cplusplus
-	#include <termios.h> // for getch() and kbhit()
-	#include <unistd.h> // for getch(), kbhit() and (u)sleep()
-	#include <sys/ioctl.h> // for getkey()
-	#include <sys/types.h> // for kbhit()
-	#include <sys/time.h> // for kbhit()
-
-/// Function: getch
-/// Get character without waiting for Return to be pressed.
-/// Windows has this in conio.h
-RLUTIL_INLINE int getch(void) {
-	// Here be magic.
-	struct termios oldt, newt;
-	int ch;
-	tcgetattr(STDIN_FILENO, &oldt);
-	newt = oldt;
-	newt.c_lflag &= ~(ICANON | ECHO);
-	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-	ch = getchar();
-	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-	return ch;
-}
-
-/// Function: kbhit
-/// Determines if keyboard has been hit.
-/// Windows has this in conio.h
-RLUTIL_INLINE int kbhit(void) {
-	// Here be dragons.
-	static struct termios oldt, newt;
-	int cnt = 0;
-	tcgetattr(STDIN_FILENO, &oldt);
-	newt = oldt;
-	newt.c_lflag    &= ~(ICANON | ECHO);
-	newt.c_iflag     = 0; // input mode
-	newt.c_oflag     = 0; // output mode
-	newt.c_cc[VMIN]  = 1; // minimum time to wait
-	newt.c_cc[VTIME] = 1; // minimum characters to wait for
-	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-	ioctl(0, FIONREAD, &cnt); // Read count
-	struct timeval tv;
-	tv.tv_sec  = 0;
-	tv.tv_usec = 100;
-	select(STDIN_FILENO+1, NULL, NULL, NULL, &tv); // A small time delay
-	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-	return cnt; // Return number of characters
-}
-#endif // _WIN32
 
 #ifndef gotoxy
 /// Function: gotoxy
