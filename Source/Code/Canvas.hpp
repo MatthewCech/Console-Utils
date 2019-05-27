3c00b372148e74e4d3afc3dfe4aa29c969cca49b
Matthew Cech <contact@matthewcech.com>
2019-05-26 17:01:45 -0700

    = Console condensing

--- Source/Code/Canvas.hpp
+++ Source/Code/Canvas.hpp
@@ -1,15 +1,55 @@
 #pragma once
-#include "CanvasRaster.hpp"
 #include "Field2D.hpp"
+#include "Colors.hpp"
 
 namespace RConsole
 {
   #define DEFAULT_WIDTH (rlutil::tcols() - 1)
   #define DEFAULT_HEIGHT rlutil::trows()
-}
 
-namespace RConsole
-{
+  // The raster info struct, holds info on what is to be drawn at a location and the color.
+  struct RasterInfo
+  {
+    RasterInfo();
+    RasterInfo(const char val, Color col);
+    bool operator ==(const RasterInfo &rhs) const;
+    bool operator !=(const RasterInfo &rhs) const;
+    char Value;
+    Color C;
+  };
+
+  // Console raster class
+  class Canvas;
+  class CanvasRaster
+  {
+    friend Canvas;
+
+  public:
+    // Constructors
+    CanvasRaster(unsigned int width, unsigned int height);
+
+    // Method Prototypes
+    bool WriteChar(char toDraw, unsigned int x, unsigned int y, Color color = PREVIOUS_COLOR);
+    bool WriteString(const char *toWrite, size_t len, unsigned int x, unsigned int y, Color color = PREVIOUS_COLOR);
+    const Field2D<RasterInfo>& GetRasterData() const;
+    void Fill(const RasterInfo &ri);
+    void Zero();
+
+    // General
+    unsigned int GetRasterWidth() const;
+    unsigned int GetRasterHeight() const;
+
+  private:
+    // Private member functions
+    Field2D<RasterInfo>& GetRasterData();
+
+    // Variables
+    unsigned int width_;
+    unsigned int height_;
+    Field2D<RasterInfo> data_;
+
+  };
+
   class Canvas
   {
   public:
--- Source/Code/main.cpp
+++ Source/Code/main.cpp
@@ -1,36 +1,128 @@
 #include <stdio.h>
 #include <ostream>
 #include "Canvas.hpp"
-#include "RTest.hpp"
 #include "Field2D.hpp"
 #include <rlutil.h>
 #include <chrono>
 #include <thread>
 #include <csignal>
 #include "Definitions.hpp"
+#include <string>
 
 
-void TestField2D();
+
+// Defines for asserts doing floating point math
+#define RTest_NEAR_FLOAT  .0006
+#define RTest_NEAR_DOUBLE .00006
+
+// Turns on or off an assert. If there is an assert that fails, a custom exception type is thrown. 
+// Place the following line in your project or compile with the following defined: #define RTest_ASSERT_ON
+#if defined (DEBUG) | (_DEBUG)
+#define RTest_ASSERT_Active 1
+#else
+#define RTest_ASSERT_Active 0
+#endif
+
+#define RTest_ASSERT(a) do{ if(RTest_ASSERT_Active){if(!(a)) { throw(RTest::RException("Assert Failed!")); }} } while (0)                                                  
+
+
+
+namespace RTest
+{
+  // A class for timing frames.
+  class Timekeeper
+  {
+  public:
+    // Static member functions
+    static void StartFrame();
+    static void EndFrame();
+    static int GetLastTimeMS();
+    static int GetAvgTimeMS();
+    static void SetMaxSamples(unsigned int samples);
+
+  private:
+    // Frame time markers
+    static std::chrono::milliseconds startTime_;
+    static std::chrono::milliseconds endTime_;
+
+    // Other data for averaging
+    static double frameSamples_;
+    static double frameAvg_;
+    static long long lastTime_;
+    static unsigned int maxFrameSamples_;
+  };
+
+
+    //////////////////////
+   // Timekeeper Class //
+  //////////////////////
+  // Static time initialization
+  std::chrono::milliseconds Timekeeper::startTime_ = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch());
+  std::chrono::milliseconds Timekeeper::endTime_ = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch());
+  double Timekeeper::frameSamples_ = 0;
+  double Timekeeper::frameAvg_ = 0;
+  long long Timekeeper::lastTime_ = 0;
+  unsigned int Timekeeper::maxFrameSamples_ = 50;
+
+
+  // Start frame marker. Should be called at the start of a single cycle of the program.
+  void Timekeeper::StartFrame()
+  {
+    startTime_ = std::chrono::duration_cast<std::chrono::milliseconds>(
+      std::chrono::high_resolution_clock::now().time_since_epoch());
+  }
+
+
+  // End frame marker. Should be called at the end of a single cycle of the program.
+  void Timekeeper::EndFrame()
+  {
+    endTime_ = std::chrono::duration_cast<std::chrono::milliseconds>(
+      std::chrono::high_resolution_clock::now().time_since_epoch());
+
+    lastTime_ = (endTime_ - startTime_).count();
+    frameAvg_ = (frameSamples_ * frameAvg_ + lastTime_) / static_cast<double>(frameSamples_ + 1);
+    if (frameSamples_ < maxFrameSamples_)
+      frameSamples_ += 1;
+  }
+
+
+  // Get the last frame time in MS as an int.
+  int Timekeeper::GetLastTimeMS()
+  {
+    return static_cast<int>(lastTime_);
+  }
+
+
+  // Returns the average frame time that has been calculated.
+  int Timekeeper::GetAvgTimeMS()
+  {
+    return static_cast<int>(frameAvg_);
+  }
+
+
+  // Sets the total number of previous frames to take into account when averaging.
+  void Timekeeper::SetMaxSamples(unsigned int samples)
+  {
+    maxFrameSamples_ = samples;
+    frameSamples_ = 0;
+  }
+}
 
 
 // Awesome.
-using namespace RConsole;
-int main(int, char**) try
+int main(int, char**)
 {
-  // Begin with tests.
-  TestField2D();
-
   // Variables
   char letter = 'a';
 
   // Setup
-  Canvas canvas(50, 20, 3, 5);
-  Canvas::SetCursorVisible(false);
+  RConsole::Canvas canvas(50, 20, 3, 5);
+  RConsole::Canvas::SetCursorVisible(false);
   srand(0);
 
-
   // Main loop
-  while (1)
+  int cycles = 50;
+  while (--cycles > 0)
   {
     ///////////////////////////////////////////////////////////////////////////////////////
     RTest::Timekeeper::StartFrame();
@@ -57,55 +149,10 @@ int main(int, char**) try
     rlutil::setColor(rlutil::MAGENTA);
     rlutil::locate(rlutil::tcols() - 3, 1);
     printf("%3i", RTest::Timekeeper::GetAvgTimeMS());
+    rlutil::locate(rlutil::tcols() - 3, 2);
+    printf("c%2i", cycles);
   }
   
   // Return success
   return 0;
-}
-// Catch any exceptions from main.
-catch (const RTest::RException &e)
-{
-  std::cout << e << '\n';
-  throw e;
-}
-
-
-void TestField2D()
-{
-  RConsole::Field2D<int> f(5, 10);
-
-  // Test Set, increment and decrement.
-  RTest_ASSERT(f.GetIndex() == 0);
-  f.Set(5);
-  RTest_ASSERT(f.Get() == 5);
-  f.IncrementY();
-  f.Set(4);
-  RTest_ASSERT(f.GetIndex() == 5);
-  RTest_ASSERT(f.Get() == 4);
-  f.DecrementY();
-  RTest_ASSERT(f.Get() == 5);
-  f.IncrementX();
-  RTest_ASSERT(f.Get() == 0);
-  f.GoTo(1, 1);
-  f.Set(6);
-  RTest_ASSERT(f.Get() == 6);
-  f.DecrementX();
-  RTest_ASSERT(f.Get() == 4);
-
-  // 5 0
-  // 4 6
-  // Test peeking
-  RTest_ASSERT(f.Peek(0, 0) == 5);
-  RTest_ASSERT(f.Peek(0, 1) == 4);
-  RTest_ASSERT(f.Peek(1, 1) == 6);
-  RTest_ASSERT(f.Peek(1, 0) == 0);
-
-  // Test subscript operator
-  f[2][3] = 20;
-  RTest_ASSERT(f.Get() == 20);
-  f.IncrementX();
-  f.Set(23);
-  RTest_ASSERT(f.Get() == 23);
-  f[3][3] = 22;
-  RTest_ASSERT(f.Get() == 22);
 }
